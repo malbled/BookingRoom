7becc9693eca02770064052bc60b1ff89fdde7ee
Александра Малышева <leksamal@icloud.com>
2023-12-28 19:56:02 +0300

    написание тестов в репозиториях

--- BookingRoom.Repositories.Tests/Tests/ServiceReadTest.cs
+++ BookingRoom.Repositories.Tests/Tests/ServiceReadTest.cs
@@ -1,12 +1,189 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BookingRoom.Context.Tests;
+using BookingRoom.Repositories.Contracts.ReadRepositoriesContracts;
+using BookingRoom.Repositories.ReadRepositories;
+using FluentAssertions;
+using Xunit;
 
 namespace BookingRoom.Repositories.Tests.Tests
 {
-    internal class ServiceReadTest
+    public class ServiceReadTest : BookingRoomContextInMemory
     {
+        private readonly IServiceRedRepository serviceRedRepository;
+
+        public ServiceReadTest()
+        {
+            serviceRedRepository = new ServiceReadRepository(Reader);
+        }
+
+        /// <summary>
+        /// Возвращает пустой список услуг
+        /// </summary>
+        [Fact]
+        public async Task GetAllShouldReturnEmpty()
+        {
+            // Act
+            var result = await serviceRedRepository.GetAllAsync(CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEmpty();
+        }
+
+        /// <summary>
+        /// Возвращает список услуг
+        /// </summary>
+        [Fact]
+        public async Task GetAllShouldReturnValues()
+        {
+            //Arrange
+            var target = TestDataGenerator.Service();
+
+            await Context.Services.AddRangeAsync(target,
+                TestDataGenerator.Service(x => x.DeletedAt = DateTimeOffset.UtcNow));
+            await Context.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await serviceRedRepository.GetAllAsync(CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.HaveCount(1)
+                .And.ContainSingle(x => x.Id == target.Id);
+        }
+
+        /// <summary>
+        /// Получение услуги по идентификатору возвращает null
+        /// </summary>
+        [Fact]
+        public async Task GetByIdShouldReturnNull()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+
+            // Act
+            var result = await serviceRedRepository.GetByIdAsync(id, CancellationToken);
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        /// <summary>
+        /// Получение услуги по идентификатору возвращает данные
+        /// </summary>
+        [Fact]
+        public async Task GetByIdShouldReturnValue()
+        {
+            //Arrange
+            var target = TestDataGenerator.Service();
+            await Context.Services.AddAsync(target);
+            await Context.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await serviceRedRepository.GetByIdAsync(target.Id, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEquivalentTo(target);
+        }
+
+        /// <summary>
+        /// Получение списка услуг по идентификаторам возвращает пустую коллекцию
+        /// </summary>
+        [Fact]
+        public async Task GetByIdsShouldReturnEmpty()
+        {
+            //Arrange
+            var id1 = Guid.NewGuid();
+            var id2 = Guid.NewGuid();
+            var id3 = Guid.NewGuid();
+
+            // Act
+            var result = await serviceRedRepository.GetByIdsAsync(new[] { id1, id2, id3 }, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeEmpty();
+        }
+
+        /// <summary>
+        /// Получение списка услуг по идентификаторам возвращает данные
+        /// </summary>
+        [Fact]
+        public async Task GetByIdsShouldReturnValue()
+        {
+            //Arrange
+            var target1 = TestDataGenerator.Service();
+            var target2 = TestDataGenerator.Service(x => x.DeletedAt = DateTimeOffset.UtcNow);
+            var target3 = TestDataGenerator.Service();
+            var target4 = TestDataGenerator.Service();
+            await Context.Services.AddRangeAsync(target1, target2, target3, target4);
+            await Context.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await serviceRedRepository.GetByIdsAsync(new[] { target1.Id, target2.Id, target4.Id }, CancellationToken);
+
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.HaveCount(2)
+                .And.ContainKey(target1.Id)
+                .And.ContainKey(target4.Id);
+        }
+
+        /// <summary>
+        /// Поиск услуги в коллекции по идентификатору (true)
+        /// </summary>
+        [Fact]
+        public async Task IsNotNullEntityReturnTrue()
+        {
+            //Arrange
+            var target1 = TestDataGenerator.Service();
+            await Context.Services.AddAsync(target1);
+            await Context.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await serviceRedRepository.IsNotNullAsync(target1.Id, CancellationToken);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        /// <summary>
+        /// Поиск услуги в коллекции по идентификатору (false)
+        /// </summary>
+        [Fact]
+        public async Task IsNotNullEntityReturnFalse()
+        {
+            //Arrange
+            var target1 = Guid.NewGuid();
+
+            // Act
+            var result = await serviceRedRepository.IsNotNullAsync(target1, CancellationToken);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        /// <summary>
+        /// Поиск усулги в коллекции по идентификатору
+        /// </summary>
+        [Fact]
+        public async Task IsNotNullDeletedEntityReturnFalse()
+        {
+            //Arrange
+            var target1 = TestDataGenerator.Service(x => x.DeletedAt = DateTimeOffset.UtcNow);
+            await Context.Services.AddAsync(target1);
+            await Context.SaveChangesAsync(CancellationToken);
+
+            // Act
+            var result = await serviceRedRepository.IsNotNullAsync(target1.Id, CancellationToken);
+
+            // Assert
+            result.Should().BeFalse();
+        }
     }
 }
